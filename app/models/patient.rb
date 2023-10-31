5dd87e4759e3554fa19b5e19419b970fe50a54c6
harshsoni <harsh.soni@joshsoftware.com>
2023-10-31 15:01:50 +0530

    create patient and patient_record table

---
+++ app/models/patient.rb
@@ -0,0 +1,3 @@
+class Patient < ApplicationRecord
+    has_many :patient_records
+end
---
+++ db/migrate/20231031092736_create_patient_records.rb
@@ -0,0 +1,10 @@
+class CreatePatientRecords < ActiveRecord::Migration[7.1]
+  def change
+    create_table :patient_records do |t|
+      t.integer :patient_id
+
+      t.timestamps
+    end
+    add_index :patient_records, :patient_id
+  end
+end
